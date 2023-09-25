d9fa2b0d95a90f0641d1b8304abf16d057c34ca7
Naveen Raj R <108968940+Naveenraj0602@users.noreply.github.com>
2023-09-25 10:53:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXyJAggdftc1y1gcUu-SWlgwe-TaThP_A",
-  authDomain: "instagram-clone-6fa4d.firebaseapp.com",
-  projectId: "instagram-clone-6fa4d",
-  storageBucket: "instagram-clone-6fa4d.appspot.com",
-  messagingSenderId: "717508443850",
-  appId: "1:717508443850:web:f9eee947387c9d1f82fe33",
+  apiKey: "AIzaSyDe6btvzqw6sdyLjagh4lCKW06fCHGujCo",
+  authDomain: "instagram-clone-7454a.firebaseapp.com",
+  projectId: "instagram-clone-7454a",
+  storageBucket: "instagram-clone-7454a.appspot.com",
+  messagingSenderId: "1004777863880",
+  appId: "1:1004777863880:web:c7049ccab8643abb27c3fd"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
